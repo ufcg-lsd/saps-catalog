ae9d7d45b02eaf926acccf59aa2336817a7ca1e3
Wesley Monte <wesley.monte@ccc.ufcg.edu.br>
2020-07-06 16:03:45 -0300

    Updated docker/stack.sh

--- docker/stack.sh
+++ docker/stack.sh
@@ -1,248 +1,94 @@
 #!/bin/bash
 
-# Script useful for manage stack saps services life-cycle.
-# See usage for commands & options.
+readonly REPOSITORY=ufcgsaps/catalog
+readonly USAGE="usage: docker.sh [-a] {build|push|publish|run} <TAG>"
+readonly MY_PATH=$(cd "$(dirname "${0}")" || { echo "For some reason, the path is not accessible"; exit 1; }; pwd )
+readonly WORKING_DIRECTORY="$(dirname "${MY_PATH}")"
+readonly CATALOG_DOCKER_FILE_PATH="${MY_PATH}/Dockerfile"
+readonly ARTEFACT_DOCKER_FILE_PATH="${MY_PATH}/ArtefactBuilder"
 
-set -o errexit;
-
-readonly ARCHIVER_REPO=ufcgsaps/archiver
-readonly CATALOG_REPO=ufcgsaps/catalog
-readonly DISPATCHER_REPO=ufcgsaps/dispatcher
-readonly SCHEDULER_REPO=ufcgsaps/scheduler
-
-readonly ARCHIVER_CONTAINER=saps-archiver
+# Catalog Properties
 readonly CATALOG_CONTAINER=saps-catalog
-readonly DISPATCHER_CONTAINER=saps-dispatcher
-readonly SCHEDULER_CONTAINER=saps-scheduler
-readonly SAPS_NETWORK=saps-network
-
+readonly CATALOG_NETWORK=saps-network
 readonly CATALOG_USER=admin
 readonly CATALOG_PASSWORD=admin
 readonly CATALOG_DB=saps
-
 readonly CATALOG_PORT=5432
-readonly DISPATCHER_PORT=8091
 
-readonly TEMP_STORAGE_DIR=/nfs
+# Flag to indicate use of the ArtefactBuilder dockerfile.
+# Use "-a" to assign it
+artefact_flag=0
 
-build_archiver() {
-  local DOCKERFILE_DIR=docker/dockerfiles/archiver
-  local TAG="${1}"
-  docker build --tag "${ARCHIVER_REPO}":"${TAG}" \
-            --file "${DOCKERFILE_DIR}" .
-}
+build() {
+  local TAG="${1-latest}"
+  local DOCKER_FILE_PATH="${CATALOG_DOCKER_FILE_PATH}"
 
-build_catalog() {
-  local DOCKERFILE_DIR=docker/dockerfiles/catalog
-  local TAG="${1}"
-  docker build --tag "${CATALOG_REPO}":"${TAG}" \
-            --file "${DOCKERFILE_DIR}" .
-}
-
-build_dispatcher() {
-  local DOCKERFILE_DIR=docker/dockerfiles/dispatcher
-  local TAG="${1}"
-  docker build --tag "${DISPATCHER_REPO}":"${TAG}" \
-            --file "${DOCKERFILE_DIR}" .
-}
-
-build_scheduler() {
-  local DOCKERFILE_DIR=docker/dockerfiles/scheduler
-  local TAG="${1}"
-  docker build --tag "${SCHEDULER_REPO}":"${TAG}" \
-            --file "${DOCKERFILE_DIR}" .
-}
-
-check_network() {
-  local exists="$(docker network ls --format "{{.Name}}" --filter name=${SAPS_NETWORK})"
-  if [ ! "${exists}" ]; then
-    echo "Creating saps network..."
-    docker network create "${SAPS_NETWORK}"
+  if [ "${artefact_flag}" ]; then
+    DOCKER_FILE_PATH=${ARTEFACT_DOCKER_FILE_PATH}
   fi
+
+  docker build --tag "${REPOSITORY}":"${TAG}" \
+            --file "${DOCKER_FILE_PATH}" "${WORKING_DIRECTORY}"
 }
 
-run_archiver() {
-  local TAG="${1}"
-  docker run -dit \
-    --name "${ARCHIVER_CONTAINER}" \
-    --net="${SAPS_NETWORK}" --net-alias=archiver \
-    -v "$(pwd)"/config/archiver.conf:/archiver/archiver.conf \
-    -v "$(pwd)"/config/log4j.properties:/archiver/log4j.properties \
-    -v "${TEMP_STORAGE_DIR}":/archiver/nfs \
-    "${ARCHIVER_REPO}":"${TAG}"
+push() {
+  local TAG="${1-latest}"
+  docker push "${REPOSITORY}":"${TAG}"
 }
 
-run_catalog() {
-  local TAG="${1}"
+run() {
+  local TAG="${1-latest}"
   docker run -dit \
     --name "${CATALOG_CONTAINER}" \
     -p "${CATALOG_PORT}":5432 \
-    --net="${SAPS_NETWORK}" --net-alias=catalog \
+    --net="${CATALOG_NETWORK}" --net-alias=catalog \
     -v catalogdata:/var/lib/postgresql/data \
     -e POSTGRES_USER="${CATALOG_USER}" \
     -e POSTGRES_PASSWORD="${CATALOG_PASSWORD}" \
     -e POSTGRES_DB="${CATALOG_DB}" \
-    "${CATALOG_REPO}":"${TAG}"
+    "${REPOSITORY}":"${TAG}"
 }
 
-run_dispatcher() {
-  local TAG="${1}"
-  local CONF_FILE="dispatcher.conf"
-  docker run -dit \
-    --name "${DISPATCHER_CONTAINER}" \
-    -p "${DISPATCHER_PORT}":8091 \
-    --net="${SAPS_NETWORK}" --net-alias=dispatcher \
-    -v "$(pwd)"/config/"${CONF_FILE}":/dispatcher/"${CONF_FILE}" \
-    -v "$(pwd)"/config/log4j.properties:/dispatcher/log4j.properties \
-    -v "$(pwd)"/resources/execution_script_tags.json:/dispatcher/resources/execution_script_tags.json \
-    -v "${TEMP_STORAGE_DIR}":/dispatcher/nfs \
-    "${DISPATCHER_REPO}":"${TAG}"
-}
-
-run_scheduler() {
-  local TAG="${1}"
-  local CONF_FILE="scheduler.conf"
-  docker run -dit \
-    --name "${SCHEDULER_CONTAINER}" \
-    --net="${SAPS_NETWORK}" --net-alias=scheduler \
-    -v "$(pwd)"/config/"${CONF_FILE}":/scheduler/"${CONF_FILE}" \
-    -v "$(pwd)"/config/log4j.properties:/scheduler/log4j.properties \
-    -v "$(pwd)"/resources/execution_script_tags.json:/dispatcher/resources/execution_script_tags.json \
-    "${SCHEDULER_REPO}":"${TAG}"
-}
-
-access_catalog() {
-  local CATALOG_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${CATALOG_CONTAINER})
-  psql -h "${CATALOG_IP}" -p ${CATALOG_PORT} ${CATALOG_DB} ${CATALOG_USER}
-}
-
-access() {
-  local service="${1}"
-  case $service in
-    catalog)
-      access_catalog
-      ;;
-  esac
-}
-
-build() {
-  local service="${1}"
-  local tag="${2-latest}"
-  case $service in
-    archiver)
-      build_archiver ${tag}
-      ;;
-    catalog)
-      build_catalog ${tag}
-      ;;
-    dispatcher)
-      build_dispatcher ${tag}
-      ;;
-    scheduler)
-      build_scheduler ${tag}
-  esac
-}
-
-run() {
-  local service="${1}"
-  local tag="${2:-latest}"
-  check_network
-  case $service in
-    archiver)
-      run_archiver ${tag}
-      ;;
-    catalog)
-      run_catalog ${tag}
-      ;;
-    dispatcher)
-      run_dispatcher ${tag}
-      ;;
-    scheduler)
-      run_scheduler ${tag}
-  esac
-}
-
-restart_template() {
-  local CONTAINER_NAME="$1"
-  local SERVICE="$2"
-  local TAG=$(docker inspect -f '{{.Config.Image}}' ${CONTAINER_NAME} | cut -d':' -f2)
-  docker stop "${CONTAINER_NAME}"
-  docker rm "${CONTAINER_NAME}"
-  run ${SERVICE} ${TAG}
-}
-
-restart() {
-  while [[ $# -ne 0 ]]
+set_options() {
+  while getopts 'a' OPTION
   do
-    case $1 in
-      archiver) shift
-        restart_template ${ARCHIVER_CONTAINER} archiver
+    case ${OPTION} in
+      a) artefact_flag=1
         ;;
-      catalog) shift
-        restart_template ${CATALOG_CONTAINER} catalog
-        ;;
-      dispatcher) shift
-        restart_template ${DISPATCHER_CONTAINER} dispatcher
-        ;;
-      scheduler) shift
-        restart_template ${SCHEDULER_CONTAINER} scheduler
-        ;;
-      *) shift
+      ?) echo "${USAGE}" >&2
+        exit 2
         ;;
     esac
   done
 }
 
-# Stop and remove all containers
-stop() {
-  for service in ${ARCHIVER_CONTAINER} ${CATALOG_CONTAINER} ${DISPATCHER_CONTAINER} ${SCHEDULER_CONTAINER};
-  do
-    docker stop "${service}"
-    docker rm "${service}"
-  done
-}
+main() {
+  if [ "$#" -eq 0 ]; then
+      echo "Use: ${USAGE}"
+      exit 1
+  fi
 
-publish()
-{
-  local service="${1}"
-  local tag="${2:-latest}"
-  case $service in
-    archiver)
-      docker push "${ARCHIVER_REPO}":"${tag}"
-      ;;
-    catalog)
-      docker push "${CATALOG_REPO}":"${tag}"
-      ;;
-    dispatcher)
-      docker push "${DISPATCHER_REPO}":"${tag}"
-      ;;
-    scheduler)
-      docker push "${SCHEDULER_REPO}":"${tag}"
-      ;;
-  esac
-}
+  set_options "$@"
+  shift $((OPTIND - 1))
 
-define_params() {
-  case $1 in 
-    access) shift
-      access "$@"
-      ;;
+  case ${1} in
     build) shift
       build "$@"
+      ;;
+    push) shift
+      push "$@"
+      ;;
+    publish) shift
+      build "$@"
+      push "$@"
       ;;
     run) shift
       run "$@"
       ;;
-    restart) shift
-      restart "$@"
-      ;;
-    stop) shift
-      stop "$@"
-      ;;
-    publish) shift
-      publish "$@"
-      ;;
+    *)
+      echo "${USAGE}" >&2
+      exit 1
   esac
 }
 
-define_params "$@"
+main "$@"
